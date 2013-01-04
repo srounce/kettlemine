da029fa2a45f64376efdc13c8eddbfae5195565a
Samuel Rounce <me@samuelrounce.co.uk>
2013-01-04 17:29:28 +0000

    Minor fixes, getting ready to tag a version

--- routes/arduino.js
+++ routes/arduino.js
@@ -1,6 +1,6 @@
 var routePrefix = "/arduino";
 
-var route = {
+var routes = {
   index : {
     all : function( req, res ) {
       res.send('Hello arduino!');
@@ -10,11 +10,18 @@ var route = {
     all : function( req, res ) {
       var teaCounter = res.app.locals.teaCounter;
       res.send(teaCounter.toString());
+    },
+    del : function( req, res ) {
+      var teaCounter = res.app.locals.teaCounter;
+      res.send( String(teaCounter.reset() ? 1 : 0) );
     }
   }
 };
  
-exports.init = function( server ) {
-  server.all(routePrefix, route.index.all);
-  server.all(routePrefix+"/count", route.count.all);
+exports.init = function( server ) 
+{
+  server.all(routePrefix, routes.index.all);
+  
+  server.del(routePrefix+"/count", routes.count.del);
+  server.all(routePrefix+"/count", routes.count.all);
 };
