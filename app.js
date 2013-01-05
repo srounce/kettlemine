b7a20370f42e5d329dd4da57fe9e7478a0a36661
Samuel Rounce <me@samuelrounce.co.uk>
2013-01-05 13:02:43 +0000

    Added config file for storing app wide shit

--- app.js
+++ app.js
@@ -8,19 +8,23 @@ var express = require('express')
   , http = require('http')
   , path = require('path')
   , stylus = require('stylus')
-  , nib = require('nib');
+  , nib = require('nib')
+  , config = require('./config');
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 8888);
+  app.set('port', config.server.port);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+
+  app.set('title', config.strings.title);
+
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
+  app.use(express.cookieParser(config.cookies.secret));
   app.use(express.session({
     key : 'kettlemine.sid'
   }));
---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = exports = {
+  server : {
+    hostname : '0.0.0.0',
+    port : 8888
+  },
+  cookies : {
+    secret : 'CookieMonstahhhhh!!11'
+  },
+  strings : {
+    title : "Roobis Roulette"
+  }
+};
